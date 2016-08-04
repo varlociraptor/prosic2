ada3f43676428a8aa0ae3d95ad7985dbc10cc21e
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-08-04 15:48:34 +0200

    Error handling.

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ extern crate log;
 extern crate fern;
 #[macro_use]
 extern crate clap;
+extern crate csv;
+extern crate itertools;
 
 use clap::{App,AppSettings};
 
-use std::process;
+pub mod call;
+pub mod estimate;
 
-use rust_htslib::bam;
 
 fn main() {
     // parse command line
@@ -40,91 +42,6 @@ fn main() {
     }
 
     if let Some(matches) = matches.subcommand_matches("tumor-normal") {
-        // read command line parameters
-        let normal_mean_insert_size = value_t!(matches, "normal_mean_insert_size", f64).unwrap();
-        let normal_sd_insert_size = value_t!(matches, "normal_sd_insert_size", f64).unwrap();
-        let normal_heterozygosity = value_t!(matches, "normal_heterozygosity", f64).unwrap_or(0.001);
-        let ploidy = value_t!(matches, "ploidy", u32).unwrap_or(2);
-        let tumor_mean_insert_size = value_t!(matches, "tumor_mean_insert_size", f64).unwrap();
-        let tumor_sd_insert_size = value_t!(matches, "tumor_sd_insert_size", f64).unwrap();
-        let tumor_effective_mutation_rate = value_t!(matches, "tumor_effective_mutation_rate", f64).unwrap();
-        let tumor_purity = value_t!(matches, "tumor_purity", f64).unwrap_or(1.0);
-        let min_somatic_af = value_t!(matches, "min_somatic_af", f64).unwrap_or(0.05);
-        let pileup_window = value_t!(matches, "pileup_window", u32).unwrap_or(2500);
-        let normal = matches.value_of("normal").unwrap();
-        let tumor = matches.value_of("tumor").unwrap();
-
-        if let Ok(tumor_bam) = bam::IndexedReader::new(&tumor) {
-            if let Ok(normal_bam) = bam::IndexedReader::new(&normal) {
-                let genome_size = (0..tumor_bam.header.target_count()).fold(0, |s, tid| {
-                    s + tumor_bam.header.target_len(tid).unwrap() as u64
-                });
-
-                // init tumor sample
-                let tumor_sample = libprosic::Sample::new(
-                    tumor_bam,
-                    pileup_window,
-                    libprosic::InsertSize {
-                        mean: tumor_mean_insert_size,
-                        sd: tumor_sd_insert_size
-                    },
-                    libprosic::priors::TumorModel::new(
-                        ploidy,
-                        tumor_effective_mutation_rate,
-                        genome_size,
-                        tumor_purity,
-                        normal_heterozygosity
-                    ),
-                    libprosic::likelihood::LatentVariableModel::new(tumor_purity)
-                );
-
-                // init normal sample
-                let normal_sample = libprosic::Sample::new(
-                    normal_bam,
-                    pileup_window,
-                    libprosic::InsertSize {
-                        mean: normal_mean_insert_size,
-                        sd: normal_sd_insert_size
-                    },
-                    libprosic::priors::InfiniteSitesNeutralVariationModel::new(
-                        ploidy,
-                        normal_heterozygosity
-                    ),
-                    libprosic::likelihood::LatentVariableModel::new(1.0)
-                );
-
-                // init joint model
-                let mut joint_model = libprosic::model::ContinuousVsDiscreteModel::new(
-                    tumor_sample,
-                    normal_sample
-                );
-
-                // setup events
-                let events = [
-                    libprosic::case_control::Event{
-                        name: "GERMLINE".to_owned(),
-                        af_case: 0.0..1.0,
-                        af_control: vec![0.5, 1.0]
-                    },
-                    libprosic::case_control::Event{
-                        name: "SOMATIC".to_owned(),
-                        af_case: min_somatic_af..1.0,
-                        af_control: vec![0.0]
-                    }
-                ];
-
-                // perform calling
-                if let Err(msg) = libprosic::case_control::call(&"-", &"-", &events, &mut joint_model) {
-                    error!("{}", msg);
-                    process::exit(1);
-                }
-            } else {
-                error!("Error reading normal BAM.");
-                process::exit(1);
-            }
-        } else {
-            error!("Error reading tumor BAM.");
-            process::exit(1);
-        }
+        call::tumor_normal(matches);
     }
 }

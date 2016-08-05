56c3f1778d422921c7d671cb2c521bea0b0885eb
Johannes Köster <johannes.koester@tu-dortmund.de>
2016-08-05 13:23:09 +0200

    Set minimum and maximum allele frequency for estimation.

--- src/estimate.rs
+++ src/estimate.rs
@@ -7,9 +7,11 @@ use csv;
 use libprosic::estimation::effective_mutation_rate;
 
 pub fn effective_mutation_rate(matches: &clap::ArgMatches) -> Result<(), Box<Error+Send+Sync>> {
+    let min_af = value_t!(matches, "min-af", f64).unwrap_or(0.12);
+    let max_af = value_t!(matches, "max-af", f64).unwrap_or(0.25);
     let mut reader = csv::Reader::from_reader(io::stdin());
     let freqs = try!(reader.decode().collect::<Result<Vec<f64>, _>>());
-    let estimate = effective_mutation_rate::Estimator::train(freqs);
+    let estimate = effective_mutation_rate::Estimator::train(freqs.into_iter().filter(|&f| f >= min_af && f <= max_af));
 
     // print estimated mutation rate to stdout
     println!("{}", estimate.effective_mutation_rate());
